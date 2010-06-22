384286e088e02f294be5d5232afffc3c24b4f61a
kimura.shunya <kimura.shunya@565d200c-1ae3-11df-99a4-2df76caecac5>
2010-06-22 03:12:31 +0000

    comment done

--- trunk/src/net/broomie/utils/Tokenizer.java
+++ trunk/src/net/broomie/utils/Tokenizer.java
@@ -1,3 +1,21 @@
+/**
+* Copyright 2010 Shunya KIMURA <brmtrain@gmail.com>
+*
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*/
+
 package net.broomie.utils;
 
 import java.util.ArrayList;
@@ -45,11 +63,11 @@ public class Tokenizer {
     private StringTagger tagger;
 
     /**
-     * Constructor for Tokenizer.
+     * The constructor for Tokenizer class.
+     * @param senConfPath Specify the path for sen configuration.
      */
     public Tokenizer(String senConfPath) {
         try {
-            //tagger = StringTagger.getInstance(SEN_CONF_PATH);
             tagger = StringTagger.getInstance(senConfPath);
         } catch (Exception e) {
             e.printStackTrace();
