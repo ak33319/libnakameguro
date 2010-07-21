587b9c8b3c2a22d0dd9fd1bdc11a0a2763baadfb
kimura.shunya <kimura.shunya@565d200c-1ae3-11df-99a4-2df76caecac5>
2010-07-21 09:36:55 +0000

    add single word count

--- src/net/broomie/utils/SingleTokenizer.java
+++ src/net/broomie/utils/SingleTokenizer.java
@@ -6,22 +6,22 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.EnumSet;
 import java.util.Properties;
-
-
-
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.*;
 
 
 /**
  * Tokenize for Japanese document.
  * @author kimura
-  */
+ */
 public final class SingleTokenizer {
 
-/**
- * Tokenize Japanese document.
- * @param args The arguments from command-line.
- * @throws IOException io exception.
- */
+    /**
+     * Tokenize Japanese document.
+     * @param args The arguments from command-line.
+     * @throws IOException io exception.
+     */
     public static void main(String[] args) throws IOException {
         GoSenTokenizer tokenizer;
         Properties prop = new Properties();
@@ -32,12 +32,22 @@ public final class SingleTokenizer {
             new BufferedReader(new InputStreamReader(
                     new FileInputStream(args[0])));
         String line;
+	HashMap<String, Integer> map = new HashMap<String, Integer>(1000000);
         while ((line = in.readLine()) != null) {
             String[] rv = tokenizer.getToken(line,
                     EnumSet.of(GoSenTokenizer.ExtractType.Noun));
             for (String token : rv) {
-                System.out.println(token);
+		if (!map.containsKey(token)) {
+		    map.put(token, 1);
+		} else {
+		    map.put(token, map.get(token).intValue() + 1);
+		}
             }
         }
+	Iterator itr = map.keySet().iterator();
+	while(itr.hasNext()) {
+	    Object token = itr.next();
+	    System.out.println(token + "\t" + map.get(token));
+	}
     }
 }
